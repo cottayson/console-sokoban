66f6d48bbf85d23ed24c9501f29e16615d1b25c1
cottayson <cottayson@gmail.com>
2020-12-02 22:48:58 +0700

    Load Super Sokoban (SNES-J) Level and create game loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,59 @@
 #include <iostream>
 #include <vector>
+#include <string>
 #include <cassert>
+#include <conio.h> // for keypress
 #include "board.hpp"
 #include "testing.hpp"
 #include "utils.hpp"
+#define WIDE true
 
 using namespace std;
 
+template <typename TCell>
+void gameLoop(Board<TCell>& f, std::string startState, bool wide=false) {
+  f.loadFromString(startState);
+  cout << f.toString(wide) << endl;
+  while(true) {
+    char key = _getch();
+    // cout << key << endl;
+    switch (key) {
+      case 'w': f.movePlayerByDiff(-1, 0); break;
+      case 's': f.movePlayerByDiff(+1, 0); break;
+      case 'a': f.movePlayerByDiff(0, -1); break;
+      case 'd': f.movePlayerByDiff(0, +1); break;
+      case 'r': f.loadFromString(startState); break;
+      case 'p': return;
+      default: break;
+    }
+    cout << f.toString(wide) << endl;
+  }
+}
+
 int main() {
-  Board<ushort> f(7, 5);
-  Board<ushort> g(7, 5);
+  Board<ushort> f(17, 16);
   cout << "width = " << f.width << ", height = " << f.height << endl;
-  f.loadFromString(
-    "#######"
-    "#  @  #"
-    "#  $  #"
-    "#     #"
-    "#######"
-  );
+  std::string startString = 
+  "            #### "
+  "###### #### #  # "
+  "#    ### @###  # "
+  "# $$ $ # $$    # "
+  "#   $#  $ $ #$###"
+  "####.### #$ $ $ #"
+  "   #.....#  #   #"
+  "####..##.##$  ###"
+  "#  #.....   ###  "   
+  "#  ....#$####    "
+  "#  ##### #       "   
+  "##$###   #       "   
+  "#  ## $  #       "   
+  "#    $ $##       "   
+  "#  ##   #        "   
+  "#########        ";
+
+
+  gameLoop(f, startString, WIDE);
   
-  cout << f.player_i << " " << f.player_j << endl;
-  
-  cout << f.toString() << endl;
-  
-  test();
+  // test();
   return 0;
 }
